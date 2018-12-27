ekohl/metadata_json_deps

commit fbc34868142b87b2b0e57734d850cd62e9714f41

Improve the gemspec

metadata_json_deps.gemspec
@@ -2,11 +2,12 @@ Gem::Specification.new do |s|
   s.name        = 'metadata_json_deps'
   s.version     = '0.1.0'
   s.licenses    = ['MIT']
-  s.summary     = 'Check your Puppet metadata'
+  s.summary     = 'Check your Puppet metadata dependencies'
   s.description = 'Verify all your dependencies allow the latest versions on Puppet Forge'
   s.authors     = ['Ewoud Kohl van Wijngaarden']
   s.email       = 'ewoud+rubygems@kohlvanwijngaarden.nl'
   s.files       = Dir['lib/**/*.rb'] + Dir['bin/*']
+  s.extra_rdoc_files = ['README']
   s.homepage    = 'https://github.com/ekohl/metadata_json_deps'
   s.metadata    = { 'source_code_uri' => 'https://github.com/ekohl/metadata_json_deps' }
   s.executables << 'metadata-json-deps'

commit 0e3af14457829787c6a68c6f4de1ca9b6a525572

Correct the extra_rdoc_files
Regression from fbc34868142b87b2b0e57734d850cd62e9714f41

metadata_json_deps.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |s|
   s.authors     = ['Ewoud Kohl van Wijngaarden']
   s.email       = 'ewoud+rubygems@kohlvanwijngaarden.nl'
   s.files       = Dir['lib/**/*.rb'] + Dir['bin/*']
-  s.extra_rdoc_files = ['README']
+  s.extra_rdoc_files = ['README.md']
   s.homepage    = 'https://github.com/ekohl/metadata_json_deps'
   s.metadata    = { 'source_code_uri' => 'https://github.com/ekohl/metadata_json_deps' }
   s.executables << 'metadata-json-deps'