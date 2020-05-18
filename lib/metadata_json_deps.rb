551dadfd1cfffb43e82ac44ddae7ce340c0ee537
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-18 16:26:29 +0200

    Move everything into a single file

--- lib/metadata_json_deps.rb
+++ lib/metadata_json_deps.rb
@@ -1,4 +1,48 @@
+require 'puppet_forge'
+require 'puppet_metadata'
+require 'semantic_puppet'
+
 module MetadataJsonDeps
-  autoload :ForgeVersions, 'metadata_json_deps/forge_versions'
-  autoload :Runner, 'metadata_json_deps/runner'
+  class ForgeVersions
+    def initialize(cache = {})
+      @cache = cache
+    end
+
+    def get_current_version(name)
+      name = name.sub('/', '-')
+      @cache[name] ||= get_version(get_mod(name))
+    end
+
+    private
+
+    def get_mod(name)
+      PuppetForge::Module.find(name)
+    end
+
+    def get_version(mod)
+      SemanticPuppet::Version.parse(mod.current_release.version)
+    end
+  end
+
+  def self.run(filenames, verbose = false)
+    forge = ForgeVersions.new
+
+    filenames.each do |filename|
+      puts "Checking #{filename}"
+      metadata = PuppetMetadata.read(filename)
+
+      metadata.dependencies.map do |dependency, constraint|
+        current = forge.get_current_version(dependency)
+
+        if constraint.include?(current)
+          if verbose
+            puts "  #{dependency} (#{constraint}) matches #{current}"
+          end
+        else
+          puts "  #{dependency} (#{constraint}) doesn't match #{current}"
+        end
+      end
+    end
+  rescue Interrupt
+  end
 end
